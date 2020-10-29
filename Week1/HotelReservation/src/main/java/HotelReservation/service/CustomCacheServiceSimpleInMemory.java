85219469b7bdd0251e830c440ba079e331b3d486
jurczakn <nickolas.jurczak@gmail.com>
2020-10-29 11:41:13 -0400

    added custom cache and began test set up

---
+++ Week1/HotelReservation/src/main/java/HotelReservation/service/CustomCacheServiceSimpleInMemory.java
@@ -0,0 +1,74 @@
+package HotelReservation.service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class CustomCacheServiceSimpleInMemory <T> implements CustomCacheService<T> {
+	
+	private Set<T> cache = new HashSet<T>();
+	
+	public CustomCacheServiceSimpleInMemory(Set<T> cache) {
+		super();
+		this.cache = cache;
+	}
+
+	public CustomCacheServiceSimpleInMemory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setCache(Set<T> cache) {
+		this.cache = cache;
+	}
+
+	@Override
+	public void addToCache(T obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeFromCache(T obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T retrieveItem(T obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void emptyCache() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void updateFromCache(T org, T upd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<T> retrieveAllItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<T> retrieveMatching(Predicate<T> p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ Week1/HotelReservation/src/test/java/HotelReservation/service/test/CustomCacheServiceSimpleInMemoryTest.java
@@ -0,0 +1,72 @@
+package HotelReservation.service.test;
+
+import static org.junit.Assert.fail;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import HotelReservation.pojos.Guest;
+import HotelReservation.pojos.Room;
+import HotelReservation.pojos.Room.RoomType;
+import HotelReservation.service.CustomCacheServiceSimpleInMemory;
+
+public class CustomCacheServiceSimpleInMemoryTest {
+	
+	private CustomCacheServiceSimpleInMemory<Guest> cacheService;
+	
+	static private Set<Guest> testCache;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		testCache = new HashSet<>();
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+		Room room1 = new Room(2,false, RoomType.DOUBLE_BED, true, 101);
+		
+		Room room2 = new Room(1, true, RoomType.SINGLE_BED, false, 201);
+		
+		Guest guest1 = new Guest("Jack Skelton", room1, "555-555-5555", 200.22);
+		
+		Guest guest2 = new Guest("Николас Юрча", room1, "246-189-1987", 65.32);
+		
+		Guest guest3 = new Guest("Norm Normal", room2, "3365951122", 0.0);
+		
+		Guest guest4 = new Guest(null, null, null, 0.0);
+		
+		testCache.add(guest1);
+		testCache.add(guest2);
+		testCache.add(guest3);
+		testCache.add(guest4);
+		
+		cacheService = new CustomCacheServiceSimpleInMemory<Guest>(testCache);
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+		testCache.clear();
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
