cde3e3b12284326679376825785378b83d7bede4
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-01 14:44:59 -0500

    Created tests for Spell Service

--- P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/services/SpellServiceImplTest.java
+++ P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/services/SpellServiceImplTest.java
@@ -1,7 +1,9 @@
 package SpellPointTracker.services;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -9,16 +11,24 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 //import org.apache.log4j.Logger;
 
-import SpellPointTracker.services.SpellServiceImpl;
+import SpellPointTracker.pojos.Spell;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SpellServiceImplTest {
 
-    //private static Logger Log = Logger.getLogger("spellServiceLog");
+	//private static Logger Log = Logger.getLogger("spellServiceLog");
+	
+	private SpellService spellService;
+	private Spell spellOne;
+	private Spell spellTwo;
+	private Spell spellThree;
+	private List<Spell> spells;
+	private List<Spell> noSpells;
+	private List<String> spellNames;
+	private int[] spellIds;
 
     @BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -30,6 +40,20 @@ public class SpellServiceImplTest {
 
 	@Before
 	public void setUp() throws Exception {
+		spellOne = new Spell(0, "cantrip", 0, 0);
+		spellTwo = new Spell(1, "magic missle", 1, 4);
+		spellThree = new Spell(2, "fireball", 3, 12);
+		spellIds = new int[]{0, 1};
+		spellNames.add("cantrip");
+		spellNames.add("magic missle");
+		spells = new ArrayList<Spell>();
+		noSpells = new ArrayList<Spell>();
+		spells.add(spellOne);
+		spells.add(spellTwo);
+		spells.add(spellThree);
+
+		spellService = new SpellServiceImpl();
+		spellService.setAllSpells(spells);
 	}
 
 	@After
@@ -37,8 +61,20 @@ public class SpellServiceImplTest {
 	}
 
 	@Test
-	public void test() {
-        //Log.info("This is a test of the logging system!");
-		fail("Not yet implemented");
+	public void getSpellTest() {
+		spellService.setAllSpells(spells);
+		Spell s1 = spellService.getSpell(spellOne.getName());
+		assertTrue("Retrieved spell does not match expected", s1.equals(spellOne));
+
+		spellService.setAllSpells(noSpells);
+		assertTrue("Retrieve spell even though no spells stored", spellService.getSpell(spellOne.getName()).equals(null));
+	}
+
+	public void getSpellsTest() {
+		List<Spell> retrievedSpells = spellService.getSpells(spellIds);
+		assertFalse("Retrieved all spells, not specific ones", spells.size() == retrievedSpells.size());
+		assertFalse("Retrieved no spells", retrievedSpells.size() == 0);
+		assertTrue("Retrieved spells do not contain spell One", retrievedSpells.contains(spellOne));
+		assertTrue("Retrieved spells do not contain spell Two", retrievedSpells.contains(spellTwo));
 	}
 }
