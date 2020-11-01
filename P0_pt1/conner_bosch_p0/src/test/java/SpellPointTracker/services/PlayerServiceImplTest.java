a8f30d4e6b7d0139a76accf61f587ba42f087e09
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-01 14:24:47 -0500

    Created all tests for playerService

--- P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/services/PlayerServiceImplTest.java
+++ P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/services/PlayerServiceImplTest.java
@@ -1,7 +1,9 @@
 package SpellPointTracker.services;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -9,18 +11,26 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 //import org.apache.log4j.Logger;
 
-import SpellPointTracker.services.PlayerServiceImpl;
+import SpellPointTracker.pojos.Player;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PlayerServiceImplTest {
 
-    //private static Logger Log = Logger.getLogger("playerServiceLog");
+	// private static Logger Log = Logger.getLogger("playerServiceLog");
 
-    @BeforeClass
+	private PlayerService playerService;
+	private String username;
+	private String password;
+	private int level;
+	private int casterType;
+	private Player player;
+	private List<Player> players;
+	private List<Player> noPlayers;
+
+	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
@@ -30,6 +40,17 @@ public class PlayerServiceImplTest {
 
 	@Before
 	public void setUp() throws Exception {
+		username = "daveTheGamer";
+		password = "password1!";
+		level = 2;
+		casterType = 0;
+		player = new Player(1, username, password, 20, level, casterType);
+
+		players = new ArrayList<>();
+		players.add(player);
+		noPlayers = new ArrayList<>();
+
+		playerService = new PlayerServiceImpl();
 	}
 
 	@After
@@ -37,8 +58,22 @@ public class PlayerServiceImplTest {
 	}
 
 	@Test
-	public void test() {
-        //Log.info("This is a test of the logging system!");
-		fail("Not yet implemented");
+	public void createPlayerTest() {
+		assertTrue("createPlayer returned False", playerService.createPlayer(username, password, level, casterType));
+		assertTrue("Player object was not properly added to collection",
+				playerService.getPlayers().get(0).equals(player));
+
+		playerService.setPlayers(noPlayers);
+		assertFalse("createPlayer returned True for false data", playerService.createPlayer("", "", 0, 10));
+		assertTrue("false Player was added to collection", playerService.getPlayers().size() == 0);
+	}
+
+	public void getPlayerTest() {
+		playerService.setPlayers(players);
+		assertTrue("Player was not retrieved", playerService.getPlayer(username, password).equals(player));
+
+		playerService.setPlayers(noPlayers);
+		Player empty = playerService.getPlayer(username, password);
+		assertTrue("Object returned not null", empty.equals(null));
 	}
 }
