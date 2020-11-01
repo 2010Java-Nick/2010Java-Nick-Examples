01680e73fe596f08ae7287ab65e033de4a2a1874
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-01 14:44:12 -0500

    SpellService refac, include collection methods

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/services/SpellService.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/services/SpellService.java
@@ -6,6 +6,10 @@ import SpellPointTracker.pojos.Spell;
 
 public interface SpellService {
 
+    public List<Spell> getAllSpells();
+
+    public void setAllSpells(List<Spell> spells);
+
     public Spell getSpell(String spellName);
 
     public List<Spell> getSpells(int[] spellIds);
--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/services/SpellServiceImpl.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/services/SpellServiceImpl.java
@@ -2,6 +2,7 @@ package SpellPointTracker.services;
 
 import SpellPointTracker.pojos.Spell;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -9,6 +10,18 @@ import org.apache.log4j.Logger;
 public class SpellServiceImpl implements SpellService {
 
     private static Logger Log = Logger.getLogger("spellServiceLog");
+
+    List<Spell> spellCollection = new ArrayList<Spell>();
+
+    public List<Spell> getAllSpells(){
+
+        return this.spellCollection;
+
+    }
+
+    public void setAllSpells(List<Spell> spells){
+        this.spellCollection = spells;
+    }
 
     @Override
     public Spell getSpell(String spellName) {
