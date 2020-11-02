be8ec79eac921c036b7eec5d6eaddfb7c6c8fb1d
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-01 19:36:21 -0500

    Made private methods public for refactor

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/views/ConsoleUI.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/views/ConsoleUI.java
@@ -1,5 +1,10 @@
 package SpellPointTracker.views;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import org.apache.log4j.Logger;
 
 import SpellPointTracker.pojos.*;
@@ -8,12 +13,14 @@ import SpellPointTracker.controllers.SpellPointsController;
 public class ConsoleUI implements UserInterface {
 
     private SpellPointsController control;
+    private BufferedReader console;
 
     private static Logger Log = Logger.getLogger("consoleUILog");
 
-    public ConsoleUI(SpellPointsController controller) {
+    public ConsoleUI(SpellPointsController controller, BufferedReader source) {
         super();
         this.control = controller;
+        this.console = source;
     }
 
 
@@ -25,6 +32,23 @@ public class ConsoleUI implements UserInterface {
         // TODO Starts user interface, prints welcome.
         //      Ask if they would like to login or create an account.
         //      calls this.promptLogin or this.promptUserCreate
+/*         String input = "";
+
+        try {
+            input = console.readLine();
+        } catch (IOException e){
+            System.out.println("IOException thrown:" + e);
+        }
+
+        if (input == "Login") {
+            this.promptLogin();
+        }
+        else if (input == "Create") {
+            this.promptUserCreate();
+        }
+        else {
+            System.out.println("Failed!");
+        } */
 
     }
 
@@ -54,7 +78,7 @@ public class ConsoleUI implements UserInterface {
      * Asks the user for name and password
      * @return success or not
      */
-    private boolean promptLogin() {
+    public boolean promptLogin() {
         // TODO Ask if they would like to login or create an account.
         //      Then prompts user for name and password,
         //      Send to controller.setCurrentPlayer
@@ -67,7 +91,7 @@ public class ConsoleUI implements UserInterface {
      * Askes the user for name, password, level and casterType
      * @return success or not
      */
-    private boolean promptUserCreate() {
+    public boolean promptUserCreate() {
         // TODO Create prompts user for name, password, level and casterType
         //      Sent to controller.createNewPlayer
         //      if success call promptLogin
@@ -79,7 +103,7 @@ public class ConsoleUI implements UserInterface {
      * 
      * @return
      */
-    private boolean castSpell(){
+    public boolean castSpell(){
         // TODO calls controller.getAvailableSpellNames and prints them
         // asks which spell to cast and sends it to controller.castSpell
         return false;
@@ -89,7 +113,7 @@ public class ConsoleUI implements UserInterface {
      * 
      * @return
      */
-    private void rest(){
+    public void rest(){
         // TODO calls controller.rest
 
     }
