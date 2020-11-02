d9711c727f6558ccc882c418fdd748192f29ef6d
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-02 01:57:34 -0500

    Created driver to loop properly

---
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/SpellPointsDriver.java
@@ -0,0 +1,36 @@
+package SpellPointTracker;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import SpellPointTracker.controllers.SpellPointsController;
+import SpellPointTracker.services.*;
+import SpellPointTracker.views.ConsoleUI;
+
+public class SpellPointsDriver {
+
+    static BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+    static CasterService casterService = new CasterServiceImpl();
+    static PlayerService playerService = new PlayerServiceImpl();
+    static SpellService spellService = new SpellServiceImpl();
+    static CalculatorService calcService = new CalculatorService();
+
+
+    static SpellPointsController control = new SpellPointsController(casterService, playerService, spellService, calcService);
+    static ConsoleUI program = new ConsoleUI(control, console);
+
+    static Boolean pass = false;
+
+    public static void main(String[] args) {
+
+        while(!pass){
+            pass = program.startInterface();
+        }
+        pass = false;
+
+        while(!pass){
+            pass = program.promptAction();
+        }
+    }
+    
+}
