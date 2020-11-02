fee2beb4ce3b3b969d0c0b6104ca62573a7f2347
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-02 02:35:29 -0500

    Implemented methods, passes tests

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/services/CasterServiceImpl.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/services/CasterServiceImpl.java
@@ -13,7 +13,9 @@ public class CasterServiceImpl implements CasterService {
     private static Logger Log = Logger.getLogger("casterServiceLog");
 
     List<Caster> casterCollection = new ArrayList<Caster>();
-    
+    int[] levelToPoints = new int[] {0, 4, 6, 14, 17, 27, 32, 38, 44, 57, 64, 73, 73, 73, 83, 83, 94, 94, 10, 114, 123, 133};
+    int[] levelToSpell = new int[] {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9};
+
     public List<Caster> getAllCasters(){
 
         return this.casterCollection;
@@ -26,26 +28,48 @@ public class CasterServiceImpl implements CasterService {
 
     @Override
     public Caster getCaster(int casterId) {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            return casterCollection.get(casterId);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
     }
 
     @Override
     public int[] getCastersSpells(int casterId) {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            return casterCollection.get(casterId).getSpellIds();
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
     }
 
     @Override
     public int getMaxPoints(int casterId, int level) {
-        // TODO Auto-generated method stub
-        return 0;
+        switch (casterId){
+            case 0, 1, 2, 4, 6:
+                return levelToPoints[level];
+
+            case 3, 5:
+                return levelToPoints[level/2];
+
+            default:
+                return 0;
+        }
     }
 
     @Override
     public int getMaxSpellLevel(int casterId, int level) {
-        // TODO Auto-generated method stub
-        return 0;
+        switch (casterId){
+            case 0, 1, 2, 4, 6:
+                return levelToSpell[level];
+
+            case 3, 5:
+                return levelToSpell[level/2];
+
+            default:
+                return 0;
+        }
     }
     
 }
