c881f8382fe5694fa125b719d13b8e94cf06505f
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-08 12:08:22 -0500

    Built mockito and connection set up

--- P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/SpellDaoPostgresTest.java
+++ P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/SpellDaoPostgresTest.java
@@ -1,6 +1,13 @@
 package SpellPointTracker.daos;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,10 +15,28 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import SpellPointTracker.util.ConnectionUtil;
+import SpellPointTracker.pojos.Spell;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SpellDaoPostgresTest {
+
+	@Mock
+	private ConnectionUtil connUtil;
+	@Mock
+	private Connection connection;
+
+	private Connection realConn;
+	private PreparedStatement stmt;
+	private PreparedStatement testStmt;
+	private PreparedStatement spy;
+
+	private SpellDaoPostgres spellDao;
+	private Spell spell;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -23,10 +48,23 @@ public class SpellDaoPostgresTest {
 
 	@Before
 	public void setUp() throws Exception {
+		//Get new connection
+		realConn = new ConnectionUtil().createConnection();
+
+		//Set Dao with mocked ConnectionUtil
+		//spellDao = new SpellDaoPostgres(connUtil);
+
+		//Initialize spell test object
+		//spell = new Spell(1010, "big_cast", 9);
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (stmt != null) {
+			stmt.close();
+		}
+		realConn.close();
 	}
 
 	@Test
@@ -54,4 +92,17 @@ public class SpellDaoPostgresTest {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Helper method that initializes mock and spy variables using the prepared sql string provided
+	 * @param sql -Prepared SQL String
+	 */
+	private void initStmtHelper(String sql) throws SQLException {
+		//Prep Mockito Spy
+		stmt = realConn.prepareStatement(sql);
+		spy = Mockito.spy(stmt);
+
+		//Set standard connection mocking methods
+		when(connUtil.createConnection()).thenReturn(connection);
+		when(connection.prepareStatement(sql)).thenReturn(spy);
+	}
 }
