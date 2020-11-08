a974a4da1afd8e26bba0d7c6fa8a8253638cbec3
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-08 00:18:10 -0500

    Implimented tests and methods

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/PlayerDaoPostgres.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/PlayerDaoPostgres.java
@@ -3,6 +3,7 @@ package SpellPointTracker.daos;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -37,7 +38,7 @@ public class PlayerDaoPostgres implements PlayerDao {
             stmt.setInt(5, player.getCurrentLevel());
             stmt.setInt(6, player.getCasterType());
 
-            System.out.println(Integer.toString(stmt.executeUpdate()) + " inserts made");
+            stmt.executeUpdate();
 
         } catch (SQLException e) {
             Log.warn("PlayerDaoPostgres.createPlayer threw SQLException: " + e);
@@ -75,18 +76,75 @@ public class PlayerDaoPostgres implements PlayerDao {
 
     @Override
     public List<Player> readAllPlayers() throws SQLException{
-        //TODO Implement readAllPlayers
-        return null;
+
+        List<Player> players = new ArrayList<>();
+
+        try {
+            //Prep SQL for select statement
+            String sql = "SELECT * FROM player;";
+            stmt = connUtil.createConnection().prepareStatement(sql);
+
+            //Return result of SQL query
+            ResultSet rs = stmt.executeQuery();
+
+            //Loop through result set
+            while(rs.next()) {
+
+                //Read result into player and add to list
+                Player player = new Player(rs.getInt(1), 
+                                        rs.getString(2),
+                                        rs.getString(3), 
+                                        rs.getInt(4), 
+                                        rs.getInt(5),
+                                        rs.getInt(6));
+                players.add(player);
+                
+            }
+        } catch (SQLException e) {
+            Log.warn("PlayerDaoPostgres.readPlayer threw SQLException: " + e);
+            throw e;
+        }
+        return players;
     }
 
     @Override
-    public Player updatePlayer (int playerId, Player player) throws SQLException{
-        //TODO Implement updatePlayer
-        return null;
+    public void updatePlayer(Player player) throws SQLException{
+
+        try { //Prepare prepared statement
+            String sql  = "UPDATE player SET username = ?, passphrase = ?, " 
+            + "current_points = ?, current_level = ?, caster_id = ? "
+            + "WHERE player_id = ?;";
+
+            stmt = connUtil.createConnection().prepareStatement(sql);
+            stmt.setString(1, player.getUsername());
+            stmt.setString(2, player.getPassword());
+            stmt.setInt(3, player.getCurrentPoints());
+            stmt.setInt(4, player.getCurrentLevel());
+            stmt.setInt(5, player.getCasterType());
+            stmt.setInt(6, player.getId());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            Log.warn("PlayerDaoPostgres.updatePlayer threw SQLException: " + e);
+            throw e;
+        }
     }
 
     @Override
     public void deletePlayer(Player player) throws SQLException{
-        //TODO Implement deletePlayer
+		try { //Prep statement with proper SQL
+		    String sql = "DELETE FROM player "
+                    + "WHERE player_id = ?;";
+            
+            stmt = connUtil.createConnection().prepareStatement(sql);
+            stmt.setInt(1, player.getId());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            Log.warn("PlayerDaoPostgres.deletePlayer threw SQLException: " + e);
+            throw e;
+        }
     }
 }
--- P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/PlayerDaoPostgresTest.java
+++ P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/PlayerDaoPostgresTest.java
@@ -5,7 +5,9 @@ import static org.mockito.Mockito.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -49,8 +51,11 @@ public class PlayerDaoPostgresTest {
 		//Get new connection
 		realConn = new ConnectionUtil().createConnection();
 
+		//Set Dao with mocked ConnectionUtil
+		playerDao = new PlayerDaoPostgres(connUtil);
+
 		//Reinitialize player test object
-		player = new Player(1001, "Dave", "theBarbarian", 6, 2, 1);
+		player = new Player(1010, "Dave", "theBarbarian", 6, 2, 1);
 		
 	}
 
@@ -73,9 +78,6 @@ public class PlayerDaoPostgresTest {
 			fail("SQLException thrown: " + e.toString());
 		}
 
-		//Create object to be tested with mock connection containing spy
-		playerDao = new PlayerDaoPostgres(connUtil);
-
 		try {
 			playerDao.createPlayer(player);
 
@@ -89,13 +91,13 @@ public class PlayerDaoPostgresTest {
 
 			verify(spy).executeUpdate();
 
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			fail("Exception thrown: " + e);
 		} finally {
 			//Removal process, post-test
 			try {
-				testStmt = realConn.prepareStatement("DELETE FROM player "
-												+"WHERE player_id = " + Integer.toString(player.getId()));
+				testStmt = realConn.prepareStatement("DELETE FROM player WHERE player_id = ?;");
+				testStmt.setInt(1, player.getId());
 				testStmt.executeUpdate();
 			} catch (SQLException e) {
 				fail("TEST ERROR, couldn't properly remove test player!");
@@ -131,9 +133,6 @@ public class PlayerDaoPostgresTest {
 			fail("SQLException thrown: " + e.toString());
 		}
 
-		//Create object to be tested with mock connection containing spy
-		playerDao = new PlayerDaoPostgres(connUtil);
-
 		try {
 			Player resultPlayer = playerDao.readPlayer(player.getId());
 
@@ -143,14 +142,14 @@ public class PlayerDaoPostgresTest {
 
 			assertTrue("Object returned does not match expected object", player.equals(resultPlayer));
 			
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			fail("Exception thrown: " + e);
-			
+
 		} finally {
 			//Removal process, post-test
 			try {
-				testStmt = realConn.prepareStatement("DELETE FROM player "
-												+"WHERE player_id = " + Integer.toString(player.getId()));
+				testStmt = realConn.prepareStatement("DELETE FROM player WHERE player_id = ?;");
+				testStmt.setInt(1, player.getId());
 				testStmt.executeUpdate();
 			} catch (SQLException e) {
 				fail("TEST ERROR, couldn't properly remove test player!");
@@ -160,17 +159,162 @@ public class PlayerDaoPostgresTest {
 
 	@Test
 	public void testReadAllPlayers() {
-		fail("Not yet implemented");
+
+		Integer numPlayers = -1;
+
+		//Get current num of players in Database for verification
+		String sql = "SELECT COUNT(*) FROM player;";
+		try {
+			testStmt = realConn.prepareStatement(sql); 
+			ResultSet rs = testStmt.executeQuery();
+			rs.next();
+			numPlayers = rs.getInt(1);
+		} catch (SQLException e) {
+			fail("SQLException thrown in test set up: " + e.toString());
+		}
+
+		//Prep statement with proper SQL
+		sql = "SELECT * FROM player;";
+		try {
+			initStmtHelper(sql);
+		} catch (SQLException e) {
+			fail("SQLException thrown: " + e.toString());
+		}
+
+		try {
+			List<Player> allPlayers = playerDao.readAllPlayers();
+
+			//Verify statement was executed properly
+			verify(spy).executeQuery();
+
+			//Verify result set returned proper data
+			assertTrue("Returned set is not the same size as expected", numPlayers == allPlayers.size());
+			for (Player p : allPlayers){
+				assertFalse("Id returned 0 for user: " + p.getUsername(), 0 == p.getId());
+				assertFalse("Username returned blank for user number: " + p.getId(), "".equals(p.getUsername()));
+				assertFalse("Password returned blank for user: " + p.getUsername(), "".equals(p.getPassword()));
+				assertFalse("Caster type returned 0 for user: " + p.getUsername(), 0 == p.getCasterType());
+			}
+		} catch (SQLException e) {
+			fail("Exception thrown: " + e);	
+		}
 	}
 
 	@Test
 	public void testUpdatePlayer() {
-		fail("Not yet implemented");
+		//Insert test player to be read
+		//Prep statement with proper SQL
+		String sql = "INSERT INTO player VALUES " 
+					+"(?, ?, ?, ?, ?, ?);";
+		try {
+			testStmt = realConn.prepareStatement(sql); 
+			testStmt.setInt(1, player.getId());
+			testStmt.setString(2, player.getUsername());
+			testStmt.setString(3, player.getPassword());
+			testStmt.setInt(4, player.getCurrentPoints());
+			testStmt.setInt(5, player.getCurrentLevel());
+			testStmt.setInt(6, player.getCasterType());
+			assertTrue("Error in inserting test player", 1 == testStmt.executeUpdate());
+		} catch (SQLException e) {
+			fail("SQLException thrown in test set up: " + e.toString());
+		}
+
+		//Prep statement with proper SQL
+		sql = "UPDATE player SET username = ?, passphrase = ?, " 
+			+ "current_points = ?, current_level = ?, caster_id = ? "
+			+ "WHERE player_id = ?;";
+		try {
+			initStmtHelper(sql);
+		} catch (SQLException e) {
+			fail("SQLException thrown: " + e.toString());
+		}
+
+		try {
+			//Modify values 
+			player.setPassword("smash12");
+			player.setCasterType(2);
+			playerDao.updatePlayer(player);
+
+			//Verify statement was prepared properly
+			verify(spy).setString(1, player.getUsername());
+			verify(spy).setString(2, player.getPassword());
+			verify(spy).setInt(3, player.getCurrentPoints());
+			verify(spy).setInt(4, player.getCurrentLevel());
+			verify(spy).setInt(5, player.getCasterType());
+			verify(spy).setInt(6, player.getId());
+
+
+			verify(spy).executeUpdate();
+
+			//Pull modified player object from database for comparison
+			testStmt = realConn.prepareStatement("SELECT * FROM player WHERE player_id = ?;");
+			testStmt.setInt(1, player.getId());
+			ResultSet rs = testStmt.executeQuery();
+
+			rs.next();
+			Player modPlayer = new Player(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
+
+			assertEquals("Database object does not match as modified", player, modPlayer);
+
+		} catch (SQLException e) {
+			fail("Exception thrown: " + e);
+		} finally {
+			//Removal process, post-test
+			try {
+				testStmt = realConn.prepareStatement("DELETE FROM player WHERE player_id = ?;");
+				testStmt.setInt(1, player.getId());
+				testStmt.executeUpdate();
+			} catch (SQLException e) {
+				fail("TEST ERROR, couldn't properly remove test player!");
+			}
+		}
 	}
 
 	@Test
 	public void testDeletePlayer() {
-		fail("Not yet implemented");
+		//Insert test player to be read
+		//Prep statement with proper SQL
+		String sql = "INSERT INTO player VALUES " 
+					+"(?, ?, ?, ?, ?, ?);";
+		try {
+			testStmt = realConn.prepareStatement(sql); 
+			testStmt.setInt(1, player.getId());
+			testStmt.setString(2, player.getUsername());
+			testStmt.setString(3, player.getPassword());
+			testStmt.setInt(4, player.getCurrentPoints());
+			testStmt.setInt(5, player.getCurrentLevel());
+			testStmt.setInt(6, player.getCasterType());
+			assertTrue("Error in inserting test player", 1 == testStmt.executeUpdate());
+		} catch (SQLException e) {
+			fail("SQLException thrown in test set up: " + e.toString());
+		}
+
+		//Prep statement with proper SQL
+		sql = "DELETE FROM player "
+			+ "WHERE player_id = ?;";
+		try {
+			initStmtHelper(sql);
+		} catch (SQLException e) {
+			fail("SQLException thrown: " + e.toString());
+		}
+
+		//Test deletePlayer functionality
+		try {
+			playerDao.deletePlayer(player);
+
+			//Ensure proper methods called
+			verify(spy).setInt(1, player.getId());
+			verify(spy).executeUpdate();
+
+			//Attempt to delete object that was already deleted (Should throw exception)
+			testStmt = realConn.prepareStatement("DELETE FROM player WHERE player_id = ?;");
+			testStmt.setInt(1, player.getId());
+			assertEquals("Object was not deleted properly", 0, testStmt.executeUpdate());
+
+		} catch (SQLException e) {
+			fail("Exception thrown: " + e);
+
+		}  
 	}
 
 	/**
@@ -186,5 +330,4 @@ public class PlayerDaoPostgresTest {
 			when(connUtil.createConnection()).thenReturn(connection);
 			when(connection.prepareStatement(sql)).thenReturn(spy);
 	}
-
 }
