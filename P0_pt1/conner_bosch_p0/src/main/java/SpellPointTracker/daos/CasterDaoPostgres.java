c4f29f3db9d5f74e170e5afc3a207901412215ce
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-08 12:54:21 -0500

    Added connection dependency to constructor

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/CasterDaoPostgres.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/CasterDaoPostgres.java
@@ -1,11 +1,26 @@
 package SpellPointTracker.daos;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import SpellPointTracker.pojos.Caster;
+import SpellPointTracker.util.ConnectionUtil;
 
 public class CasterDaoPostgres implements CasterDao {
+    
+    private PreparedStatement stmt;
+
+    private static Logger Log = Logger.getLogger("daoLog");
+
+    private ConnectionUtil connUtil;
+
+    public CasterDaoPostgres(ConnectionUtil connectionUtil){
+        super();
+        connUtil = connectionUtil;
+    }
 
     @Override
     public void createCaster(Caster caster) throws SQLException{
--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/SpellDaoPostgres.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/SpellDaoPostgres.java
@@ -1,11 +1,26 @@
 package SpellPointTracker.daos;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import SpellPointTracker.pojos.Spell;
+import SpellPointTracker.util.ConnectionUtil;
 
 public class SpellDaoPostgres implements SpellDao {
+
+    private PreparedStatement stmt;
+
+    private static Logger Log = Logger.getLogger("daoLog");
+
+    private ConnectionUtil connUtil;
+
+    public SpellDaoPostgres(ConnectionUtil connectionUtil) {
+        super();
+        connUtil = connectionUtil;
+    }
 
     @Override
     public void createSpell(Spell spell) throws SQLException{
