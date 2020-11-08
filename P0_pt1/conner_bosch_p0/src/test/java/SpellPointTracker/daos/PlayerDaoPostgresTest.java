4f033beae48cdc2224f47ddf57fa0a7ad3849531
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-07 21:45:01 -0500

    Added create and read player tests

--- P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/PlayerDaoPostgresTest.java
+++ P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/PlayerDaoPostgresTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,26 +14,30 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import SpellPointTracker.pojos.Player;
+import SpellPointTracker.util.ConnectionUtil;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PlayerDaoPostgresTest {
 
 	@Mock
-	private static Connection connection;
+	private static ConnectionUtil connUtil;
 	@Mock
+	private static Connection connection;
+
+	private static Connection realConn;
 	private static PreparedStatement stmt;
+	private static PreparedStatement testStmt;
+	private static PreparedStatement spy;
 
 	private static PlayerDaoPostgres playerDao;
 	private Player player;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		connection = mock(Connection.class);
-		stmt = mock(PreparedStatement.class);
-		when(connection.prepareStatement(anyString())).thenReturn(stmt);
 	}
 
 	@AfterClass
@@ -41,31 +46,116 @@ public class PlayerDaoPostgresTest {
 
 	@Before
 	public void setUp() throws Exception {
-		playerDao = new PlayerDaoPostgres(connection);
-		player = new Player(1, "Dave", "theBarbarian", 6, 2, 1);
+		//Get new connection
+		realConn = new ConnectionUtil().createConnection();
+
+		//Reinitialize player test object
+		player = new Player(1001, "Dave", "theBarbarian", 6, 2, 1);
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (stmt != null) {
+			stmt.close();
+		}
+		realConn.close();
 	}
 
 	@Test
 	public void testCreatePlayerGoodInput() {
-		try {
+		//Prep statement with proper SQL
 		String sql = "INSERT INTO player VALUES " 
 					+"(?, ?, ?, ?, ?, ?);";
-		when(connection.prepareStatement(sql)).thenReturn(stmt);
-		playerDao.createPlayer(player);
-		verify(stmt).executeUpdate();
-		verify(stmt).close();
+		try {
+			initStmtHelper(sql);
+		} catch (SQLException e) {
+			fail("SQLException thrown: " + e.toString());
+		}
+
+		//Create object to be tested with mock connection containing spy
+		playerDao = new PlayerDaoPostgres(connUtil);
+
+		try {
+			playerDao.createPlayer(player);
+
+			//Verify statement was prepared properly
+			verify(spy).setInt(1, player.getId());
+			verify(spy).setString(2, player.getUsername());
+			verify(spy).setString(3, player.getPassword());
+			verify(spy).setInt(4, player.getCurrentPoints());
+			verify(spy).setInt(5, player.getCurrentLevel());
+			verify(spy).setInt(6, player.getCasterType());
+
+			verify(spy).executeUpdate();
+
 		} catch (Exception e) {
 			fail("Exception thrown: " + e);
+		} finally {
+			//Removal process, post-test
+			try {
+				testStmt = realConn.prepareStatement("DELETE FROM player "
+												+"WHERE player_id = " + Integer.toString(player.getId()));
+				testStmt.executeUpdate();
+			} catch (SQLException e) {
+				fail("TEST ERROR, couldn't properly remove test player!");
+			}
 		}
 	}
 
 	@Test
 	public void testReadPlayer() {
-		fail("Not yet implemented");
+		//Insert test player to be read
+		//Prep statement with proper SQL
+		String sql = "INSERT INTO player VALUES " 
+					+"(?, ?, ?, ?, ?, ?);";
+		try {
+			testStmt = realConn.prepareStatement(sql); 
+			testStmt.setInt(1, player.getId());
+			testStmt.setString(2, player.getUsername());
+			testStmt.setString(3, player.getPassword());
+			testStmt.setInt(4, player.getCurrentPoints());
+			testStmt.setInt(5, player.getCurrentLevel());
+			testStmt.setInt(6, player.getCasterType());
+			assertTrue("Error in inserting test player", 1 == testStmt.executeUpdate());
+		} catch (SQLException e) {
+			fail("SQLException thrown in test set up: " + e.toString());
+		}
+
+		//Prep statement with proper SQL
+		sql = "SELECT * FROM player " 
+			+ "WHERE player_id = ?;";
+		try {
+			initStmtHelper(sql);
+		} catch (SQLException e) {
+			fail("SQLException thrown: " + e.toString());
+		}
+
+		//Create object to be tested with mock connection containing spy
+		playerDao = new PlayerDaoPostgres(connUtil);
+
+		try {
+			Player resultPlayer = playerDao.readPlayer(player.getId());
+
+			//Verify statement was prepared and executed properly
+			verify(spy).setInt(1, player.getId());
+			verify(spy).executeQuery();
+
+			assertTrue("Object returned does not match expected object", player.equals(resultPlayer));
+			
+		} catch (Exception e) {
+			fail("Exception thrown: " + e);
+			
+		} finally {
+			//Removal process, post-test
+			try {
+				testStmt = realConn.prepareStatement("DELETE FROM player "
+												+"WHERE player_id = " + Integer.toString(player.getId()));
+				testStmt.executeUpdate();
+			} catch (SQLException e) {
+				fail("TEST ERROR, couldn't properly remove test player!");
+			}
+		}
 	}
 
 	@Test
@@ -81,6 +171,20 @@ public class PlayerDaoPostgresTest {
 	@Test
 	public void testDeletePlayer() {
 		fail("Not yet implemented");
+	}
+
+	/**
+	 * Helper method that initializes static variables using the prepared sql string provided
+	 * @param sql -Prepared SQL String
+	 */
+	private void initStmtHelper(String sql) throws SQLException {
+			//Prep Mockito Spy
+			stmt = realConn.prepareStatement(sql);
+			spy = Mockito.spy(stmt);
+
+			//Set standard connection mocking methods
+			when(connUtil.createConnection()).thenReturn(connection);
+			when(connection.prepareStatement(sql)).thenReturn(spy);
 	}
 
 }
