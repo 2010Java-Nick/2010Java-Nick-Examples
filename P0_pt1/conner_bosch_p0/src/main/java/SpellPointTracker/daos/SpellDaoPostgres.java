bdf7762ff89e54e33c4d7d24532deb226285c171
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-08 13:42:49 -0500

    Implimented create and read spell methods

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/SpellDaoPostgres.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/SpellDaoPostgres.java
@@ -1,6 +1,7 @@
 package SpellPointTracker.daos;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -24,13 +25,44 @@ public class SpellDaoPostgres implements SpellDao {
 
     @Override
     public void createSpell(Spell spell) throws SQLException{
-        //TODO Implement createSpell
+        try {
+            String sql = "INSERT INTO spell VALUES "
+                        +"(?, ?, ?);";
+            stmt = connUtil.createConnection().prepareStatement(sql);
+            stmt.setInt(1, spell.getId());
+            stmt.setString(2, spell.getName());
+            stmt.setInt(3, spell.getLevel());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            Log.warn("SpellDaoPostgres.createSpell threw SQLException: " + e);
+            throw e;
+        }
     }
 
     @Override
     public Spell readSpell(int spellId) throws SQLException{
-        //TODO Implement readSpell
-        return null;
+        try {
+            //Prep SQL for select statement
+            String sql = "SELECT * FROM spell "
+                        + "WHERE spell_id = ?;";
+
+            stmt = connUtil.createConnection().prepareStatement(sql);
+            stmt.setInt(1, spellId);
+
+            //Return result of SQL query
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+
+            //Read result into spell
+            Spell spell = new Spell(rs.getInt(1), 
+                                    rs.getString(2),
+                                    rs.getInt(3));
+            return spell;
+        } catch (SQLException e) {
+            Log.warn("SpellDaoPostgres.readSpell threw SQLException: " + e);
+            throw e;
+        }
     }
 
     @Override
