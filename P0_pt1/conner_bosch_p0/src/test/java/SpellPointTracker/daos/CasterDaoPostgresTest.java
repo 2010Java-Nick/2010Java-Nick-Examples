284fd3308beacab6ac7f945f95d4f5777f64ce00
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-08 15:53:59 -0500

    Built test environment

--- P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/CasterDaoPostgresTest.java
+++ P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/CasterDaoPostgresTest.java
@@ -1,6 +1,13 @@
 package SpellPointTracker.daos;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,10 +15,28 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import SpellPointTracker.util.ConnectionUtil;
+import SpellPointTracker.pojos.Caster;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CasterDaoPostgresTest {
+
+	@Mock
+	private ConnectionUtil connUtil;
+	@Mock
+	private Connection connection;
+
+	private Connection realConn;
+	private PreparedStatement stmt;
+	private PreparedStatement testStmt;
+	private PreparedStatement spy;
+
+	private CasterDaoPostgres casterDao;
+	private Caster caster;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -23,10 +48,22 @@ public class CasterDaoPostgresTest {
 
 	@Before
 	public void setUp() throws Exception {
+		//Get new connection
+		realConn = new ConnectionUtil().createConnection();
+
+		//Set Dao with mocked ConnectionUtil
+		casterDao = new CasterDaoPostgres(connUtil);
+
+		//Initialize spell test object
+		caster = new Caster(1010, "Illithid", new int[] {1, 2});
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (stmt != null) {
+			stmt.close();
+		}
+		realConn.close();
 	}
 
 	@Test
@@ -52,6 +89,20 @@ public class CasterDaoPostgresTest {
 	@Test
 	public void testDeleteCaster() {
 		fail("Not yet implemented");
+	}
+
+	/**
+	 * Helper method that initializes mock and spy variables using the prepared sql string provided
+	 * @param sql -Prepared SQL String
+	 */
+	private void initStmtHelper(String sql) throws SQLException {
+		//Prep Mockito Spy
+		stmt = realConn.prepareStatement(sql);
+		spy = Mockito.spy(stmt);
+
+		//Set standard connection mocking methods
+		when(connUtil.createConnection()).thenReturn(connection);
+		when(connection.prepareStatement(sql)).thenReturn(spy);
 	}
 
 }
