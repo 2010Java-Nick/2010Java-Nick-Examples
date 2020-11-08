30bca3ddb664c3c158a9fdfeb0db80563743c4cb
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-07 21:45:42 -0500

    Implimented createPlayer

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/PlayerDaoPostgres.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/PlayerDaoPostgres.java
@@ -1,36 +1,69 @@
 package SpellPointTracker.daos;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import SpellPointTracker.pojos.Player;
+import SpellPointTracker.util.ConnectionUtil;
 
 public class PlayerDaoPostgres implements PlayerDao {
 
-    @Override
-    public void createPlayer(Player player){
-        //TODO Implement createPlayer
+    private PreparedStatement stmt;
+
+    private static Logger Log = Logger.getLogger("daoLog");
+
+    private ConnectionUtil connUtil;
+
+    public PlayerDaoPostgres(ConnectionUtil connectionUtil){
+        super();
+        connUtil = connectionUtil;
     }
 
     @Override
-    public Player readPlayer(int playerId){
+    public void createPlayer(Player player) throws SQLException{
+
+        try {
+            String sql = "INSERT INTO player VALUES "
+                        +"(?, ?, ?, ?, ?, ?);";
+            stmt = connUtil.createConnection().prepareStatement(sql);
+            stmt.setInt(1, player.getId());
+            stmt.setString(2, player.getUsername());
+            stmt.setString(3, player.getPassword());
+            stmt.setInt(4, player.getCurrentPoints());
+            stmt.setInt(5, player.getCurrentLevel());
+            stmt.setInt(6, player.getCasterType());
+
+            System.out.println(Integer.toString(stmt.executeUpdate()) + " inserts made");
+
+        } catch (SQLException e) {
+            Log.warn("PlayerDaoPostgres.createPlayer threw SQLException: " + e);
+            throw e;
+        }
+    }
+
+    @Override
+    public Player readPlayer(int playerId) throws SQLException{
         //TODO Implement readPlayer
         return null;
     }
 
     @Override
-    public List<Player> readAllPlayers(){
+    public List<Player> readAllPlayers() throws SQLException{
         //TODO Implement readAllPlayers
         return null;
     }
 
     @Override
-    public Player updatePlayer (int playerId, Player player){
+    public Player updatePlayer (int playerId, Player player) throws SQLException{
         //TODO Implement updatePlayer
         return null;
     }
 
     @Override
-    public void deletePlayer(Player player){
+    public void deletePlayer(Player player) throws SQLException{
         //TODO Implement deletePlayer
     }
 }
