25c2eee23c6d7bb6a54a975656e32682a57ef4a8
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-08 19:18:34 -0500

    Added create and read caster methods

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/CasterDaoPostgres.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/daos/CasterDaoPostgres.java
@@ -1,7 +1,12 @@
 package SpellPointTracker.daos;
 
+import java.sql.Array;
+import java.sql.CallableStatement;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -12,6 +17,7 @@ import SpellPointTracker.util.ConnectionUtil;
 public class CasterDaoPostgres implements CasterDao {
     
     private PreparedStatement stmt;
+    private CallableStatement callStmt;
 
     private static Logger Log = Logger.getLogger("daoLog");
 
@@ -24,13 +30,68 @@ public class CasterDaoPostgres implements CasterDao {
 
     @Override
     public void createCaster(Caster caster) throws SQLException{
-        //TODO Implement createCaster
+        try(Connection conn = connUtil.createConnection()) {
+
+            conn.setAutoCommit(false);
+            Array ids = conn.createArrayOf("int4", caster.getSpellIds());
+            stmt = conn.prepareStatement("INSERT INTO caster VALUES (?, ?, ?); ");
+            stmt.setInt(1, caster.getId());
+            stmt.setString(2, caster.getName());
+            stmt.setBoolean(3, caster.getHalfCaster());
+            stmt.executeUpdate();
+
+            callStmt = conn.prepareCall("CALL insert_caster_spell_list(?, ?); ");
+            callStmt.setInt(1, caster.getId());
+            callStmt.setArray(2, ids);
+            callStmt.execute();
+
+            conn.commit();
+
+        } catch (SQLException e) {
+            Log.warn("CasterDaoPostgres.createCaster threw SQLException: " + e);
+            throw e;
+        }
     }
 
     @Override
     public Caster readCaster(int casterId) throws SQLException{
-        //TODO Implement readCaster
-        return null;
+
+        try(Connection conn = connUtil.createConnection()) {
+
+            //Prep SQL for select statement
+            String sql = "SELECT c.caster_id, c.caster_name, c.half_caster, cs.spell_id FROM caster c "
+                        + "INNER JOIN caster_spell cs " 
+                        + "ON c.caster_id = cs.caster_id " 
+                        + "WHERE c.caster_id = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, casterId);
+            
+            //Return result of SQL query
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+
+            //Read result into variables
+            Integer castId = rs.getInt(1);
+            String castName = rs.getString(2);
+            Boolean castHalf = rs.getBoolean(3);
+
+            //Initialize spellIds container
+            List<Integer> ids = new LinkedList<Integer>();
+
+            //Add spellIds to list
+            do {
+                ids.add(rs.getInt(4));
+            } while (rs.next());
+
+            //Convert list to array
+            Integer[] spellIds = ids.toArray(new Integer[ids.size()]);
+
+            return new Caster(castId, castName, castHalf, spellIds);
+
+        } catch (SQLException e) {
+            Log.warn("CasterDaoPostgres.readCaster threw SQLException: " + e);
+            throw e;
+        }
     }
 
     @Override
