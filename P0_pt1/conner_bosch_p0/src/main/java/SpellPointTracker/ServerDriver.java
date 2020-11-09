b7cd0ea1167cc3cdf749eae9f232a3023ff90a50
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-09 16:59:11 -0500

    Added more RESTful calls

--- P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/ServerDriver.java
+++ P0_pt1/conner_bosch_p0/src/main/java/SpellPointTracker/ServerDriver.java
@@ -25,26 +25,28 @@ public class ServerDriver {
         try {
             app = Javalin.create().start(8113);
 
-            app.post("/game-management/user/login", ctx -> webControl.promptLogin(ctx));
+            app.get("/game-management/user/login", ctx -> webControl.promptLogin(ctx));
             app.post("/game-management/user/create", ctx -> webControl.promptUserCreate(ctx));
             app.get("/game-management/user/spells/available", ctx -> webControl.getSpells(ctx));
-            app.post("/game-management/user/spell/cast/:name", ctx -> webControl.castSpell(ctx));
-            app.get("game-management/user/rest", ctx -> webControl.rest(ctx));
+            app.post("/game-management/user/spell/cast/:spell", ctx -> webControl.castSpell(ctx));
+            app.post("game-management/user/rest", ctx -> webControl.rest(ctx));
             app.get("game-management/user/status", ctx -> webControl.getStatus(ctx));
 
             app.get("/data-management/players", ctx -> webAdmin.getPlayers(ctx));
             app.get("/data-management/player/:id", ctx -> webAdmin.getPlayer(ctx));
             app.put("/data-management/player/:id", ctx -> webControl.updatePlayer(ctx));
-            app.post("/data-management/player/:id", ctx -> webControl.promptUserCreate(ctx));
+            app.post("/data-management/player/:id", ctx -> webAdmin.createPlayer(ctx));
             app.delete("/data-management/player/:id", ctx -> webControl.deletePlayer(ctx));
 
             app.get("/data-management/casters", ctx -> webControl.getAllCasters(ctx));
             app.get("/data-management/caster/:id", ctx -> webControl.getCaster(ctx));
+            app.put("/data-management/caster/:id", ctx -> webAdmin.updateCaster(ctx));
             app.post("/data-management/caster/:id", ctx -> webAdmin.postCaster(ctx));
             app.delete("/data-management/caster/:id", ctx -> webAdmin.deleteCaster(ctx));
 
             app.get("/data-management/spells", ctx -> webAdmin.getSpells(ctx));
             app.get("/data-management/spell/:name", ctx -> webControl.getSpell(ctx));
+            app.put("/data-management/spell/:id", ctx -> webAdmin.updateSpell(ctx));
             app.post("/data-management/spell", ctx -> webAdmin.postSpell(ctx));
             app.delete("/data-management/spell/:id", ctx -> webAdmin.deleteSpell(ctx));
 
