d5f53e661a51f5d3e6ac6b9b28666af4839a61d0
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-08 19:18:22 -0500

    Added create and read caster tests

--- P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/CasterDaoPostgresTest.java
+++ P0_pt1/conner_bosch_p0/src/test/java/SpellPointTracker/daos/CasterDaoPostgresTest.java
@@ -3,11 +3,12 @@ package SpellPointTracker.daos;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.sql.Array;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.After;
@@ -32,9 +33,14 @@ public class CasterDaoPostgresTest {
 	private Connection connection;
 
 	private Connection realConn;
-	private PreparedStatement stmt;
 	private PreparedStatement testStmt;
+	
+	private PreparedStatement stmt;
+	private CallableStatement callStmt;
+	
 	private PreparedStatement spy;
+	private CallableStatement callSpy;
+	private Array ids;
 
 	private CasterDaoPostgres casterDao;
 	private Caster caster;
@@ -56,7 +62,7 @@ public class CasterDaoPostgresTest {
 		casterDao = new CasterDaoPostgres(connUtil);
 
 		//Initialize spell test object
-		caster = new Caster(1010, "Illithid", false, new int[] {1, 2});
+		caster = new Caster(1010, "Illithid", false, new Integer[] {1, 2});
 	}
 
 	@After
@@ -71,12 +77,11 @@ public class CasterDaoPostgresTest {
 	public void testCreateCaster() {
 		try {
 			//Prep statement with proper SQL
-			String sql = "BEGIN; INSERT INTO caster VALUES "
-						+"(?, ?, ?); "
-						+"CALL insert_caster_spell_list(?, ARRAY?); " 
-						+"COMMIT; ";
+			String sql1 = "INSERT INTO caster VALUES (?, ?, ?); ";
+			String sql2 = "CALL insert_caster_spell_list(?, ?); ";
 			try {
-				initStmtHelper(sql);
+				initStmtHelper(sql1);
+				initCallableHelper(sql2);
 			} catch (SQLException e) {
 				fail("SQLException thrown in test setup: " + e);
 			}
@@ -85,16 +90,16 @@ public class CasterDaoPostgresTest {
 			try {
 				casterDao.createCaster(caster);
 
-				//Verify statement was prepared properly
+				//Verify prepared statement was called properly
 				verify(spy).setInt(1, caster.getId());
 				verify(spy).setString(2, caster.getName());
-				verify(spy).setBoolean(3, false);
-
-				//Verify statement set caster_spell values properly
-				verify(spy).setInt(4, caster.getId());
-				verify(spy).setString(5, Arrays.toString(caster.getSpellIds()));
-
+				verify(spy).setBoolean(3, caster.getHalfCaster());
 				verify(spy).executeUpdate();
+
+				//Verify callable statement was called properly
+				verify(callSpy).setInt(1, caster.getId());
+				verify(callSpy).setArray(2, ids);
+				verify(callSpy).execute();
 
 			} catch (SQLException e) {
 				fail("SQLException thrown in creation process: " + e);
@@ -108,13 +113,65 @@ public class CasterDaoPostgresTest {
 			} catch (SQLException e) {
 				fail("TEST ERROR, could not properly remove caster: " + e);
 			}
-
 		}
 	}
 
 	@Test
 	public void testReadCaster() {
-		fail("Not yet implemented");
+		try {
+			//Insert test caster to be read
+			String sql = "INSERT INTO caster VALUES "
+						+"(?, ?, ?); "
+						+"INSERT INTO caster_spell VALUES "
+						+"(?, ?), (?, ?);";
+			try {
+				testStmt = realConn.prepareStatement(sql);
+				testStmt.setInt(1, caster.getId());
+				testStmt.setString(2, caster.getName());
+				testStmt.setBoolean(3, caster.getHalfCaster());
+				testStmt.setInt(4, caster.getId());
+				testStmt.setInt(5, caster.getSpellIds()[0]);
+				testStmt.setInt(6, caster.getId());
+				testStmt.setInt(7, caster.getSpellIds()[1]);
+				assertTrue("Error in inserting test caster", 1 == testStmt.executeUpdate());
+			} catch (SQLException e) {
+				fail("SQLException thrown in test setup: " + e);
+			}
+
+			//Prep statement with proper SQL
+			sql = "SELECT c.caster_id, c.caster_name, c.half_caster, cs.spell_id FROM caster c "
+				+ "INNER JOIN caster_spell cs " 
+				+ "ON c.caster_id = cs.caster_id " 
+				+ "WHERE c.caster_id = ?";
+
+			try {
+				initStmtHelper(sql);
+			} catch (SQLException e ) {
+				fail ("SQLException thrown; " + e);
+			}
+
+			try {
+				Caster resultCaster = casterDao.readCaster(caster.getId());
+
+				//Verify statement was prepared and executed properly
+				verify(spy).setInt(1, caster.getId());
+				verify(spy).executeQuery();
+
+				assertTrue("Object returned does not match expected object", caster.equals(resultCaster));
+			
+			} catch (SQLException e) {
+				fail("SQLException was thrown: " + e);
+			}
+		} finally {
+			//Removal process, post-test
+			try {
+				testStmt = realConn.prepareStatement("DELETE FROM caster WHERE caster_id = ?;");
+				testStmt.setInt(1, caster.getId());
+				testStmt.executeUpdate();
+			} catch (SQLException e) {
+				fail("TEST ERROR, could not properly remove caster: " + e);
+			}
+		}
 	}
 
 	@Test
@@ -141,9 +198,23 @@ public class CasterDaoPostgresTest {
 		stmt = realConn.prepareStatement(sql);
 		spy = Mockito.spy(stmt);
 
+
 		//Set standard connection mocking methods
 		when(connUtil.createConnection()).thenReturn(connection);
 		when(connection.prepareStatement(sql)).thenReturn(spy);
 	}
 
+	private void initCallableHelper(String sql) throws SQLException {
+		//Prep Mockito callSpy
+		callStmt = realConn.prepareCall(sql);
+		callSpy = Mockito.spy(callStmt);
+
+		when(connUtil.createConnection()).thenReturn(connection);
+		when(connection.prepareCall(sql)).thenReturn(callSpy);
+
+		ids = realConn.createArrayOf("int4", caster.getSpellIds());
+		when(connection.createArrayOf("int4", caster.getSpellIds())).thenReturn(ids);
+
+	}
+ 
 }
