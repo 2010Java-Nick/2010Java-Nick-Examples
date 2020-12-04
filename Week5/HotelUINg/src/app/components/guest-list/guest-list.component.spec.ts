9fe67623f71cfd4b4d6b770e82d3df74c8898ccd
jurczakn <nickolas.jurczak@gmail.com>
2020-12-04 16:30:58 -0500

    mocking

--- Week5/HotelUINg/src/app/components/guest-list/guest-list.component.spec.ts
+++ Week5/HotelUINg/src/app/components/guest-list/guest-list.component.spec.ts
@@ -1,6 +1,27 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Pipe, PipeTransform } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Guest } from 'src/app/models/guest';
+import { Transform } from 'stream';
 
 import { GuestListComponent } from './guest-list.component';
+
+@Pipe({
+  name: 'guestFilter'
+})
+class MockGuestFilterPipe implements PipeTransform {
+
+  transform(guestList: Guest[], value: string): Guest[] {
+    
+    if (value === '123') {
+      return [ { firstName: '', lastName: '', roomNumber: 123, phoneNumber: ''} ];
+    }
+    
+    return [];
+  }
+
+}
+
 
 describe('GuestListComponent', () => {
   let component: GuestListComponent;
@@ -8,9 +29,11 @@ describe('GuestListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GuestListComponent ]
+      declarations: [GuestListComponent,
+        MockGuestFilterPipe],
+      imports: [HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- Week5/HotelUINg/src/app/services/guest.service.spec.ts
+++ Week5/HotelUINg/src/app/services/guest.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 import { GuestService } from './guest.service';
 
@@ -6,7 +7,9 @@ describe('GuestService', () => {
   let service: GuestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(GuestService);
   });
 
