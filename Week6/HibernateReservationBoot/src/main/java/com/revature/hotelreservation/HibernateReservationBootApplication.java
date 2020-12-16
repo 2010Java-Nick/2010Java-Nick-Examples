407a41866c769d190b538df83b26e1cb85309c5f
jurczakn <nickolas.jurczak@gmail.com>
2020-12-16 12:55:34 -0500

    removed jps from boot

--- Week6/HibernateReservationBoot/src/main/java/com/revature/hotelreservation/HibernateReservationBootApplication.java
+++ Week6/HibernateReservationBoot/src/main/java/com/revature/hotelreservation/HibernateReservationBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+//@EnableJpaRepositories
 public class HibernateReservationBootApplication {
 
 	public static void main(String[] args) {
